d91ae4ffa660617b0ea9093dd286bef411b0cf2f
David Herrmann <davherrmann@googlemail.com>
2022-04-13 12:16:16 +0200

    add more tests for vectors

--- raytracing/vector_test.go
+++ raytracing/vector_test.go
@@ -52,6 +52,23 @@ func TestVectorAdd(t *testing.T) {
 	}
 }
 
+func TestVectorSubtract(t *testing.T) {
+	tests := []struct {
+		A        Vec
+		B        Vec
+		Expected Vec
+	}{
+		{A: Vec{X: 1}, B: Vec{X: 1}, Expected: Vec{X: 0}},
+		{A: Vec{X: -1}, B: Vec{X: 1}, Expected: Vec{X: -2}},
+		{A: Vec{X: 1, Y: 1, Z: 1}, B: Vec{X: 2, Y: 3, Z: 4}, Expected: Vec{X: -1, Y: -2, Z: -3}},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Subtract(test.B)
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
 func TestVectorMultiply(t *testing.T) {
 	tests := []struct {
 		A        Vec
@@ -82,6 +99,87 @@ func TestVectorDot(t *testing.T) {
 
 	for _, test := range tests {
 		actual := test.A.Dot(test.B)
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
+func TestVectorCross(t *testing.T) {
+	tests := []struct {
+		A        Vec
+		B        Vec
+		Expected Vec
+	}{
+		{A: Vec{X: 1, Y: 2, Z: 3}, B: Vec{X: 4, Y: 5, Z: 6}, Expected: Vec{X: -3, Y: 6, Z: -3}},
+		{A: Vec{X: -1, Y: 2, Z: -3}, B: Vec{X: 4, Y: 5, Z: 6}, Expected: Vec{X: 27, Y: -6, Z: -13}},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Cross(test.B)
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
+func TestVectorLength(t *testing.T) {
+	tests := []struct {
+		A        Vec
+		Expected float64
+	}{
+		{A: Vec{X: 2, Y: 2, Z: 1}, Expected: 3},
+		{A: Vec{X: -1, Y: 2, Z: -2}, Expected: 3},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Length()
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
+func TestVectorNormalized(t *testing.T) {
+	tests := []struct {
+		A        Vec
+		Expected Vec
+	}{
+		{A: Vec{X: 2}, Expected: Vec{X: 1}},
+		{A: Vec{X: -1, Y: 2, Z: -2}, Expected: Vec{X: -1. / 3, Y: 2. / 3, Z: -2. / 3}},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Normalized()
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
+func TestVectorReflect(t *testing.T) {
+	tests := []struct {
+		A        Vec
+		Normal   Vec
+		Expected Vec
+	}{
+		{A: Vec{X: 2}, Normal: Vec{X: -1}, Expected: Vec{X: -2}},
+		{A: Vec{X: 2, Y: 1}, Normal: Vec{X: -1}, Expected: Vec{X: -2, Y: 1}},
+		{A: Vec{X: 2, Y: 1, Z: 3}, Normal: Vec{X: -1}, Expected: Vec{X: -2, Y: 1, Z: 3}},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Reflect(test.Normal)
+		requireEqual(t, actual, test.Expected)
+	}
+}
+
+func TestVectorRefract(t *testing.T) {
+	tests := []struct {
+		A                 Vec
+		Normal            Vec
+		IndexOfRefraction float64
+		Expected          Vec
+	}{
+		{A: Vec{X: 1}, Normal: Vec{X: -1}, IndexOfRefraction: 1, Expected: Vec{X: 1}},
+		{A: Vec{X: 1}, Normal: Vec{X: -1}, IndexOfRefraction: 2, Expected: Vec{X: 1}},
+		{A: Vec{X: 1, Y: 1}, Normal: Vec{X: -1}, IndexOfRefraction: 0, Expected: Vec{X: 1, Y: 0}},
+	}
+
+	for _, test := range tests {
+		actual := test.A.Refract(test.Normal, test.IndexOfRefraction)
 		requireEqual(t, actual, test.Expected)
 	}
 }
